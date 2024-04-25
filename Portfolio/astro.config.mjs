f1a84d033a4b51fca669c7c1d794f561b917a98b
Arnoldo Trujillo <29070400arnoltrujillo.asuncion@gmail.com>
2024-04-25 13:17:27 -0600

    configuracion

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+
+  site: 'https://arnoldo-trujillo.dev',
+
   // ...
   output: 'hybrid',
   adapter: vercel({
