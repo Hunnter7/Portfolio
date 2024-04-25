c71ca3e2983bc0c7158e8682c06c1604e03b56a4
Arnoldo Trujillo <29070400arnoltrujillo.asuncion@gmail.com>
2024-04-25 11:03:26 -0600

    Astro vercel Serverless

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';\
+import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel({
-      webAnalytics: { enabled: true }
-    }),
+  // ...
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
-
